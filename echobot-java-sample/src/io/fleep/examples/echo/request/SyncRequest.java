5e94b528ed6ce682be72f271e6a65509ad412447
Märt Konsap <mrt.konsap@gmail.com>
2017-09-24 00:24:35 +0300

    Deleted everything except fleepclient, which I want to use in my project.

--- echobot-java-sample/src/io/fleep/examples/echo/request/SyncRequest.java
+++
@@ -1,8 +0,0 @@
-package io.fleep.examples.echo.request;
-
-public class SyncRequest extends BaseRequest {
-
-	public SyncRequest(String ticket) {
-		super("account/sync", ticket);
-	}
-}
--- ios-mac-library+samples/FleepReader/FleepReader/FLLocalization.h
+++
@@ -1,1 +0,0 @@
-#define FLLocalize(token, default) default
